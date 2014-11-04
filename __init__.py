50bbdd2255fe1d5d1d32143e26c786424569aeca
Jordi Esteve <jesteve@zikzakmedia.com>
2014-11-04 12:38:30 +0100

    Add cost field in timesheet lines

--- __init__.py
+++ __init__.py
@@ -3,9 +3,11 @@
 # the full copyright notices and license terms.
 from trytond.pool import Pool
 from .work import *
+from .line import *
 
 
 def register():
     Pool.register(
         Work,
+        Line,
         module='timesheet_cost_revenue', type_='model')
