f66cd66e21b8e64bd6a7b7de037052eee45aa839
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:49:53 +0100

    Cleanup tests

--- tests/test_timesheet_cost_revenue.py
+++ tests/test_timesheet_cost_revenue.py
@@ -2,37 +2,23 @@
 # This file is part timesheet_cost_revenue module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class TimesheetCostRevenueTestCase(unittest.TestCase):
-    '''
-    Test Timesheet Cost Revenue module.
-    '''
+    'Test Timesheet Cost Revenue module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('timesheet_cost_revenue')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('timesheet_cost_revenue')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
